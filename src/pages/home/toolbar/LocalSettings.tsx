227728cb172b7cbb0fe1d080715d0caf8cc0c69d
iSweeTiamo <103113557+li-peifeng@users.noreply.github.com>
2025-01-29 12:06:51 +0100

    更新 LocalSettings.tsx

--- src/pages/home/toolbar/LocalSettings.tsx
+++ src/pages/home/toolbar/LocalSettings.tsx
@@ -131,7 +131,7 @@ export const LocalSettings = () => {
                   localStorage.removeItem("hope-ui-color-mode")
                   notificationService.show({
                     status: "success",
-                    description: "设置成功，即将自动刷新",
+                    description: "{t("home.toolbar.local_settings_auto")}",
                     closable: false,
                   })
                   setTimeout(function () {
